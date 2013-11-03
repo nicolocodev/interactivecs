865b94111fceb70f2d7a0c5d4b4a32ea728c772f
nicolocodev <nicoherreraf@hotmail.com>
2013-11-03 11:34:55 -0500

    Se removio el proyecto de Windows 81

--- InteractivecsWin8/MainPage.xaml.cs
+++ InteractivecsWin8/MainPage.xaml.cs
@@ -1,56 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System;
+﻿using Windows.System;
 using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace InteractivecsWin8
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         private string _currentDirectory;
-        
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
             Loaded += OnLoaded;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
             _currentDirectory = ">";
-            richTextBoxPrincipal.Document.SetText(new TextSetOptions(),"Hola mundo");
+            richTextBoxPrincipal.Document.SetText(new TextSetOptions(), _currentDirectory);
         }
 
         private void PrincipalOnKeyDown(object sender, KeyRoutedEventArgs e)
         {
+            string currentLine;
             switch (e.Key)
             {
                 case VirtualKey.Back:
-                    
+                    richTextBoxPrincipal.Document.GetText(TextGetOptions.IncludeNumbering, out currentLine);
+                    if (currentLine == _currentDirectory)
+                        e.Handled = true;
                     break;
                 case VirtualKey.Enter:
                     
                     break;
                 case VirtualKey.Left:
-                    
+
                     break;
             }
         }
